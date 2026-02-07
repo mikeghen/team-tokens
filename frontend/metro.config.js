8f15ae085d3ec4a9cddfc497a16bd7ae71b49f29
Michael Ghen <mike@mikeghen.com>
2026-02-07 16:42:26 -0500

    Working in ios emulator

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -3,4 +3,25 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver = {
+	...config.resolver,
+	// Silence export-map warnings from dependencies that deep-import CJS files.
+	// Metro will fall back to file-based resolution instead.
+	unstable_enablePackageExports: false,
+};
+
+// Force CJS interop for WalletConnect/Reown packages to avoid
+// "Cannot assign to property 'default' which has only a getter" under new arch.
+config.transformer = {
+	...config.transformer,
+	getTransformOptions: async () => ({
+		transform: {
+			experimentalImportSupport: false,
+			inlineRequires: true,
+		},
+	}),
+};
+
+module.exports = config;
+
 module.exports = config;
